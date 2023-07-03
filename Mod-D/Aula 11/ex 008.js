bb68d91bf5fcc7af07a7c8f6f5415cd263650f04
clauber-DEV <clauber.porto@hotmail.com>
2023-07-03 11:26:21 -0300

    condição if no JS

--- Mod-D/Aula 11/ex 008.js
+++ Mod-D/Aula 11/ex 008.js
@@ -1,6 +1,7 @@
+/* exemplo de condição simples com comando if */
 var vel = 50.5
 console.log(`A valocidade do seu corre é ${vel}km/h`)
-if(vel > 60){
+if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permmitida. MULTADO!!`)
 }
 console.log('Dirija sempre usando cinto de segurança!.')
