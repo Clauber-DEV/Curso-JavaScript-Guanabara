8882564b285e70d19c3b076d77df031f427a13f2
clauber-DEV <clauber.porto@hotmail.com>
2023-07-06 08:52:03 -0300

    site hora do dia
    implementação de imagens no HTML
    criando laços e tratando dados na function horas() no arquivo JS

--- Mod-D/Aula 12/ex014/script.js
+++ Mod-D/Aula 12/ex014/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function horas() {
+    var mens = window.document.getElementById( 'msg')
+    var foto = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+
+    mens.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 6 && hora < 12){
+        // bom dia
+        foto.src = 'imagens/circulomanha.png'
+    }else if(hora >= 12 && hora < 18){
+        // boa tarde
+        foto.src = 'imagens/circulotarde.png'
+    }else{
+        // boa noite
+        foto.src = 'imagens/circulonoite.png'
+    }
+}
