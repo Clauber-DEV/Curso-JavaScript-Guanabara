54bac83275244ed04440134c2fd058e8ba2b1bdf
clauber-DEV <clauber.porto@hotmail.com>
2023-07-06 16:40:52 -0300

    Site de horas finalizado

--- Mod-D/Aula 12/ex014/script.js
+++ Mod-D/Aula 12/ex014/script.js
@@ -1,19 +1,26 @@
 function horas() {
-    var mens = window.document.getElementById( 'msg')
+    var mens = window.document.getElementById('msg')
+    var comp = window.document.getElementById('saudacao')
     var foto = window.document.getElementById('foto')
     var data = new Date()
     var hora = data.getHours()
 
     mens.innerHTML = `Agora são ${hora} horas.`
 
-    if(hora >= 6 && hora < 12){
-        // bom dia
+    if (hora >= 6 && hora < 12) {
+        // dia
         foto.src = 'imagens/circulomanha.png'
-    }else if(hora >= 12 && hora < 18){
-        // boa tarde
+        document.body.style.background = '#69b8f5'
+        comp.innerHTML = 'Bom dia !'
+    } else if (hora >= 12 && hora < 18) {
+        // tarde
         foto.src = 'imagens/circulotarde.png'
-    }else{
-        // boa noite
+        document.body.style.background = '#e8a13b'
+        comp.innerHTML = 'Boa tarde !'
+    } else {
+        // noite
         foto.src = 'imagens/circulonoite.png'
+        document.body.style.background = '#217074'
+        comp.innerHTML = 'Boa noite !'
     }
 }
