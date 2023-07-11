d404a594d65796c97ff7ac859b3ea97699a77b8f
clauber-DEV <clauber.porto@hotmail.com>
2023-07-11 15:59:24 -0300

    mais atualizado

--- Mod-D/Aula 12/ex015/script.js
+++ Mod-D/Aula 12/ex015/script.js
@@ -1,50 +1,61 @@
-function verificar(){
+function verificar() {
     var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    var img = window.document.getElementById('fotos')
-    if (fano.value.length == 0 || Number(fano.value) > ano){
-        window.alert('[ERRO] Verifique os dados e tente novamente! ')
-    } else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
+    var anoAtual = data.getFullYear()
+    var anoNasc = document.getElementById('anoNasc')
+    var result = window.document.getElementById('res')
+
+    if (anoNasc.value.length == 0 || Number(anoNasc.value) > anoAtual) {
+        window.alert('[ERRO] Verifique os dados e preencha novamente! ')
+    } else {
+        var sexo = document.getElementsByName('sexo')
+        var idade = anoAtual - Number(anoNasc.value)
         var genero = ''
-        
-        img.setAttribute('id', 'foto')
-        if(fsex[0].checked) {
+        if (sexo[0].checked) {
             genero = 'Homem'
-            if (idade >=0 && idade < 10){
-                //criança
-                img.src = 'imgs\bebem.png'
-            } else if (idade < 21){
-                // Jovem
-                img.setAttribute('src', 'O jovem.png')
-            }else if (idade < 60){
-                // adulto
-                img.setAttribute('src', 'homem.png')
-            }else {
-                // idoso
-                img.setAttribute('src', 'idoso.png')
-            }
-        } else if (fsex[1].checked){
+        } else if (sexo[1].checked) {
             genero = 'Mulher'
-            if (idade >=0 && idade < 10){
+        }
+        var foto = window.document.getElementById('fotos')
+
+        if (genero == 'Homem') {
+
+            if (idade > 0 && idade < 12) {
                 //criança
-                img.setAttribute('src', 'A bebe.png')
-            } else if (idade < 21){
+                result.innerHTML = `Detectamos um ${genero = 'menino'} com ${idade} anos.`
+                foto.src = 'imagens/Mbebe.png'
+            } else if (idade < 21) {
                 // Jovem
-                img.setAttribute('src', 'A jovem.png')
-            }else if (idade < 60){
+                result.innerHTML = `Detectamos um ${genero = 'adolecente'} com ${idade} anos.`
+                foto.src = 'imagens/Mjovem.png'
+            } else if (idade < 65) {
                 // adulto
-                img.setAttribute('src', 'mulher.png')
-            }else {
+                result.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                foto.src = 'imagens/Mhomem.png'
+            } else {
                 // idoso
-                img.setAttribute('src', 'idosa.png')
+                result.innerHTML = `Detectamos um ${genero = 'idoso'} com ${idade} anos.`
+                foto.src = 'imagens/Midoso.png'
+            }
+        } else if (genero == 'Mulher') {
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+                result.innerHTML = `Detectamos uma ${genero = 'menina'} com ${idade} anos.`
+                foto.src = 'imagens/Fbebe.png'
+            } else if (idade < 21) {
+                // Jovem
+                result.innerHTML = `Detectamos uma ${genero = 'adolecente'} com ${idade} anos.`
+                foto.src = 'imagens/Fjovem.png'
+            } else if (idade < 60) {
+                // adulto
+                result.innerHTML = `Detectamos uma ${genero = 'mulher'} com ${idade} anos.`
+                foto.src = 'imagens/Fmulher.png'
+            } else {
+                // idoso
+                result.innerHTML = `Detectamos uma ${genero = 'idosa'} com ${idade} anos.`
+                foto.src = 'imagens/Fidosa.png'
             }
         }
-        res.style.textAlign = `center`
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        //res.appendChild(img)
+        //res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
