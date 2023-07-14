2306ff78667c00097fd711b290c5abf87a0e1e5d
clauber-DEV <clauber.porto@hotmail.com>
2023-07-13 22:51:28 -0300

    aula 13
    estruturas de repetição

---
+++ Mod-E/teste.js
@@ -0,0 +1,23 @@
+// estrutura de repetição com teste lógico no final. do while == faça enquanto
+var num = 1
+do {
+    console.log(`${num} repetição.`)
+    num++
+} while (num <= 6)
+
+
+// estrutura de repetição com teste lógico no início. while == enquanto 
+var num = 1
+while (num <= 6) {
+    console.log(`${num} Oi. tudo bem?`)
+    num++
+}
+
+
+
+
+// repetição sem estrutura
+console.log(`OI tudo bem?`)
+console.log(`OI tudo bem?`)
+console.log(`OI tudo bem?`)
+console.log(`OI tudo bem?`)
