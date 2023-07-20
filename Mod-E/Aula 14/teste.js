1a3e4ec66536ca7c13f75c0a8ef252a277c3efc5
clauber-DEV <clauber.porto@hotmail.com>
2023-07-20 09:35:49 -0300

    Aula 14
    repetição utilizando  "for"

---
+++ Mod-E/Aula 14/teste.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+console.log(`Vai começar!`)
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log(`FIm!`)
+/*Para executar o depurador deve-se ir em executar iniciar e depurador ou ctrl + shift + D 
+marque o brack point no inicio do codigo, mande executar o código e aperte f10 para ir passo a passo.*/
