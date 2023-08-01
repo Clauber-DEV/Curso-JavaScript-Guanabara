3ba9385983058cb64032d1e330b0acca8be3f493
clauber-DEV <clauber.porto@hotmail.com>
2023-08-01 08:59:42 -0300

    Exercício 17
    site de tabuada

--- Mod-E/Aula 14/Aula 14 ex/ex 017/script.js
+++ Mod-E/Aula 14/Aula 14 ex/ex 017/script.js
@@ -1,5 +1,18 @@
-function multiplicar(){
+function multiplicar() {
     let numero = window.document.querySelector('input#numero')
     let tabuada = window.document.querySelector('select#tabuada')
-    
+    if (numero.value.length == 0) {
+        window.alert("[ERRO] Por favor digite um número!")
+    } else {
+        let num = Number(numero.value)
+        let res = 0
+        tabuada.innerHTML = ``
+        for (let n = 1; n <= 10; n++) {
+            let opcao = window.document.createElement(`option`)
+            res = n * num
+            opcao.text = `${num} x ${n} = ${res}`
+            opcao.value = `tab${n}`
+            tabuada.appendChild(opcao)
+        }
+    }
 }
