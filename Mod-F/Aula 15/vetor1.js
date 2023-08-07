349031dbb596f24218d86707eddfafec70fd8e72
clauber-DEV <clauber.porto@hotmail.com>
2023-08-07 17:59:58 -0300

    Aula 15

--- Mod-F/Aula 15/vetor1.js
+++ Mod-F/Aula 15/vetor1.js
@@ -1,9 +1,9 @@
 // Noção básica de vetores, pequenas funções e comandos
 num = [4, 6, 7, 8, 9]
-num[5] = 6  // para acrescentar um elemento no erry == vetor no indice indicado, no caso indice[5]
-num.push(7) // comando push == empurrar para ecrescentar um elemento e mais um indice no erry
-num.length  // para informar a quantidade de elementos em um erry.
-num.sort()  // para ordenar os elementos do erry em ordem crescente.
+num[5] = 6  // para acrescentar um elemento no array == vetor no indice indicado, no caso indice[5]
+num.push(7) // comando push == empurrar para ecrescentar um elemento e mais um indice no array
+num.length  // para informar a quantidade de elementos em um array.
+num.sort()  // para ordenar os elementos do array em ordem crescente.
 num.indexOf() // para mostrar em qual indice fica o valor desejado.
 
 // forma de utilizar o comando indexOf()
